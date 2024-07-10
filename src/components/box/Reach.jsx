705f82e9c68544f096a3dcb04313dc430ad8fe00
ramgore <ramgore150@gmail.com>
2024-07-10 16:34:44 +0530

    Update Reach.jsx
    sm:text-6xl

--- src/components/box/Reach.jsx
+++ src/components/box/Reach.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Reach = ({number, title}) => {
   return (
     <div className='flex flex-col justify-center gap-y-3 '>
-          <p className='text-6xl text-4xl font-extrabold self-center text-gray-300'>{number}</p>
+          <p className='sm:text-6xl text-4xl font-extrabold self-center text-gray-300'>{number}</p>
           <p className='lg:text-xl  font-bold self-center'>{title}</p>
     </div>
   )
